d29bb4c4873c8ec29270e5fd984870559292ec82
G4tten <ludovica.gatti04@gmail.com>
2024-10-16 09:34:08 +0200

    Inizio classe giocatore

--- Yahtzee.py
+++ Yahtzee.py
@@ -7,3 +7,16 @@ screen_width = 800;
 screen_height = 600; 
 
 screen = pygame.display.set_mode((screen_width, screen_height)) #creazione della finestra di gioco
+
+#Le istruzioni che seguono non verranno eseguite sulla finestra di gioco ma sul terminale di Visual Studio
+class giocatore:
+
+    def __init__(self, username):
+        self.username = username
+        #tabellone con categorie
+        #punteggio totale del giocatore
+
+
+giocatore1 = input("Inserire il nome del giocatore1: ")
+
+print(f"Giocatore1: {giocatore1}")
