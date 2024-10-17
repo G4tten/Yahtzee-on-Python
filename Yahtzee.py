76f5cbb7606bb7a1d37bda2c313628a4e790044c
G4tten <ludovica.gatti04@gmail.com>
2024-10-17 12:02:28 +0200

    Aggiornamento finestra di gioco

--- Yahtzee.py
+++ Yahtzee.py
@@ -7,15 +7,37 @@ screen_width = 800;
 screen_height = 600; 
 
 screen = pygame.display.set_mode((screen_width, screen_height)) #creazione della finestra di gioco
+pygame.display.set_caption("Yahtzee Game")
+
+#Palette di colori che pensavo di usare:
+#Teal -> rgb(37, 113, 128)
+#Beige -> rgb(242, 229, 191)
+#Orange -> rgb(253, 139, 81)
+#Bordeaux -> rgb(203, 96, 64)
+
+background = (242, 229, 191)
+
+run = True
+while run: #game loop
+
+    screen.fill(background)
+
+    for event in pygame.event.get(): #gestore di eventi
+        if event.type == pygame.QUIT: #Quando viene cliccata la x della finestra il gioco si chiude (fondamentale per uscire dal ciclo infinito)
+            run = False
+    
+    pygame.display.flip() #per aggiornare lo schermo
+
+pygame.quit() #Termina il programma
 
 #Le istruzioni che seguono non verranno eseguite sulla finestra di gioco ma sul terminale di Visual Studio
-class giocatore:
+# class giocatore:
 
-    def __init__(self, username, punteggio, scheda):
+def __init__(self, username, punteggio, scheda):
         self.username = username
 
-        #tabellone con categorie
-        #punteggio totale del giocatore
+        # tabellone con categorie
+        # punteggio totale del giocatore
 
 
 giocatore1 = input("Inserire il nome del giocatore1: ")
@@ -25,11 +47,12 @@ print(f"Giocatore1: {giocatore1}")
 ################################################################################
 
 #Classe dado, contiene gli attributi di valore e la funzione del lancio dadi
+
 class Dado:
 
     def __init__(self):
         self.valore= 0
-    
+
     def lancio_dadi(self):
         self.valore= random.randint(1,6)
         return self.valore
